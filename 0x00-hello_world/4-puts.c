bc8aeb447de3d7df0cf6ba90958dbb4291062e55
kevinocholah <kevinocholah@gmail.com>
2023-10-05 10:01:58 -0700

    puts inbuild

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - how to pass a string to the stdout using the puts fucn.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programmimg is like building a multilingual puzzle");
-		return (0);
+puts("\"Programmimg is like building a multilingual puzzle");
+return (0);
 }
