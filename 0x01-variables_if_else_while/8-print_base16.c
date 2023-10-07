74a203f7488632a2c0b457821230cd96784a2315
kevinocholah <kevinocholah@gmail.com>
2023-10-06 21:00:59 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  *main - Prints numbers of base 16 in lower case
+  *
+  *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
