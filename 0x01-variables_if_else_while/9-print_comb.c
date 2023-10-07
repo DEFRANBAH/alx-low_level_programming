8b243cdf10b22d3a2f2d09976450a6341f5dadd2
kevinocholah <kevinocholah@gmail.com>
2023-10-06 21:32:55 -0700

    all possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - Prints all single combination sets of single numbers
+  *
+  *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
