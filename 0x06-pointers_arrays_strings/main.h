7dc04d0b0bfb076c8e9e86bfb45b169488c45663
kevinocholah <kevinocholah@gmail.com>
2023-10-18 09:50:17 -0700

    advanced bring it on rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,10 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void print_number(int n);
+char *rot13(char *);
+
 
 #endif /* my pinky ring extra large ! yeyah 100 shots*/
